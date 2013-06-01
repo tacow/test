82944c644896f6473d45a80cfe7166d12f85c36a
yintao <yintao@localhost.localdomain>
2013-06-01 15:30:17 -0700

    template.c

---
+++ c/template.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage:\n");
+        printf("    %s []\n", argv[0]);
+        return 0;
+    }
+    return 0;
+}
+
