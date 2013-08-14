8cf14628599d23df954eb885a2b53b7d79869bcb
yintao <taco@163.com>
2013-08-14 19:45:27 +0800

    args.c

--- c/args.c
+++ c/args.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-    int i = 0;
+int main(int argc, char* argv[], char* env[]) {
+    int i = 0, j = 0;
     for(i = 0; i < argc; ++i) {
         printf("%d: %s\n", i, argv[i]);
+    }
+    printf("\n====================\n\n");
+    while(env[j]) {
+        printf("%d: %s\n", j, env[j]);
+        ++j;
     }
     return 0;
 }
