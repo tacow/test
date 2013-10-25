befa70571a95f356dd90f6ff21c81ae3831a8c23
yintao <taco@163.com>
2013-10-25 12:16:10 +0800

    client

--- c/client.c
+++ c/client.c
@@ -8,23 +8,30 @@
 int main(int argc, char* argv[]) {
     char* host;
     short port;
+    int   count = 1;
+    int   i;
+    int   s;
     struct sockaddr_in addr;
     if (argc < 3) {
-        printf("Usage: %s [host] [port]\n", argv[0]);
+        printf("Usage: %s [host] [port] [count]\n", argv[0]);
         return 0;
     }
     host = argv[1];
     port = (short)atoi(argv[2]);
+    if (argc >= 4)
+        count = atoi(argv[3]);
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     inet_aton(host, &addr.sin_addr);
     addr.sin_port = htons(port);
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    if (connect(s, (struct sockaddr*)&addr, sizeof(addr)) == 0) {
-        printf("success\n");
+    for(i = 0; i < count; ++i) {
+        printf("%d start\n", i);
+        s = socket(AF_INET, SOCK_STREAM, 0);
+        if (connect(s, (struct sockaddr*)&addr, sizeof(addr)) == 0)
+            printf("success\n");
+        else
+            printf("failed(%d): %s\n", errno, strerror(errno));
         close(s);
-    } else {
-        printf("failed(%d): %s\n", errno, strerror(errno));
     }
     return 0;
 }
