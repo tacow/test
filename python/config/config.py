4686259a77476201258d15611111574df9357323
yintao <taco@163.com>
2013-12-03 11:03:34 +0800

    config.py

--- python/config/config.py
+++ python/config/config.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python
 
 def ReadConfig(filename, sep = ":"):
-    keyVals = {}
-    f = open(filename, "r")
-    if not f:
+    try:
+        keyVals = {}
+        f = open(filename, "r")
+        if not f:
+            return keyVals
+
+        for line in f:
+            line = line.strip()
+            if line.startswith("#"):
+                continue
+            res = line.split(sep, 1)
+            if len(res) != 2:
+                continue
+            key = res[0].strip()
+            val = res[1].strip()
+            keyVals[key] = val
+
+        f.close()
         return keyVals
-
-    for line in f:
-        line = line.strip()
-        if line.startswith("#"):
-            continue
-        res = line.split(sep, 1)
-        if len(res) != 2:
-            continue
-        key = res[0].strip()
-        val = res[1].strip()
-        keyVals[key] = val
-
-    f.close()
-    return keyVals
+    except Exception, e:
+        print "Exception: %s" % str(e)
+        return {}
 
