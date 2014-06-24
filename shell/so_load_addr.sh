4785df73ade0c75fbca3a08c1b4cdc740c9923fe
Your Name <you@example.com>
2014-06-24 15:03:41 +0800

    so_load_addr.sh

--- shell/so_load_addr.sh
+++ shell/so_load_addr.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-cd /usr/lib64
+TEMP_FILE=/tmp/so_load_addr.tmp
+LIB_PATH=/usr/lib64
+
+cd ${LIB_PATH}
 
 for sofile in *.so*
 do
-    readelf -l ${sofile} > /dev/null 2>&1
+    readelf -l ${sofile} > ${TEMP_FILE} 2> /dev/null
     if [ $? -ne 0 ]
     then
         continue
     fi
     echo -n "${sofile} "
-    readelf -l ${sofile} | grep -m 1 LOAD | awk '{print $3}'
+    grep -m 1 LOAD ${TEMP_FILE} | awk '{print $3}'
 done | sort -k 2
