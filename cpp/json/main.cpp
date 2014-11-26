4553e8bb6b5396d4fae249bbba9e4989e13955ae
yintao <yintao@localhost.(none)>
2014-11-26 16:40:31 +0800

    json

--- cpp/json/main.cpp
+++ cpp/json/main.cpp
@@ -5,8 +5,10 @@
 using namespace std;
 using namespace Json;
 
+#define BOOL_TO_STRING(b) ((b) ? "true" : "false")
+
 int main() {
-    string content = "[{\"name\": \"长安街\", \"len\": 10000}, {\"name\": \"阜石路\", \"len\": 5000}]";
+    string content = "[{\"name\": \"长安街\", \"len\": 10000}, {\"name\": \"阜石路\", \"len\": 5000}, {\"name\": \"莲石路\"}]";
 
     Reader reader;
     Value root;
@@ -18,9 +20,12 @@ int main() {
     UInt size = root.size();
     for(UInt i = 0; i < size; ++i) {
         Value& item = root[i];
+        printf("Has \"name\" %s\n", BOOL_TO_STRING(item.isMember("name")));
+        printf("Has \"length\" %s\n", BOOL_TO_STRING(item.isMember("len")));
+
         string name = item["name"].asString();
         int len = item["len"].asInt();
-        printf("%s: %d\n", name.c_str(), len);
+        printf("%s: %d\n\n", name.c_str(), len);
     }
     return 0;
 }
