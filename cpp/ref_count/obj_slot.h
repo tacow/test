dafab108b5574b63da9cf82b4e6809b709ce8634
yintao <yintao@localhost.localdomain>
2015-02-12 18:35:06 +0800

    obj_slot

--- cpp/ref_count/obj_slot.h
+++ cpp/ref_count/obj_slot.h
@@ -7,39 +7,39 @@ template<class Object>
 class ObjectSlot {
 public:
     ObjectSlot() : m_obj(NULL) {
-        pthread_spin_init(&m_lock, 0);
+        pthread_mutex_init(&m_lock, NULL);
     }
 
     virtual ~ObjectSlot() {
         if (m_obj)
             m_obj->Release();
 
-        pthread_spin_destroy(&m_lock);
+        pthread_mutex_destroy(&m_lock);
     }
 
     void Set(Object* obj) {
-        pthread_spin_lock(&m_lock);
+        pthread_mutex_lock(&m_lock);
         Object* oldObj = m_obj;
         m_obj = obj;
-        pthread_spin_unlock(&m_lock);
+        pthread_mutex_unlock(&m_lock);
 
         if (oldObj)
             oldObj->Release();
     }
 
     Object* Get() {
-        pthread_spin_lock(&m_lock);
+        pthread_mutex_lock(&m_lock);
         Object* obj = m_obj;
         if (obj)
             obj->AddRef();
-        pthread_spin_unlock(&m_lock);
+        pthread_mutex_unlock(&m_lock);
 
         return obj;
     }
 
 private:
     Object* m_obj;
-    pthread_spinlock_t m_lock;
+    pthread_mutex_t m_lock;
 };
 
 #endif
