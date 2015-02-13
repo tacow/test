ae2352889ed671c786b141bf599ff5eeb9fc0cec
yintao <yintao@localhost.localdomain>
2015-02-13 17:41:34 +0800

    ref_count.h

--- cpp/ref_count/ref_count.h
+++ cpp/ref_count/ref_count.h
@@ -1,19 +1,71 @@
 #ifndef REF_COUNT_H
 #define REF_COUNT_H
 
+#include <pthread.h>
+
 template<class DerivedClass>
 class RefCountObject {
 public:
     RefCountObject(int refCount = 1) : m_refCount(refCount) {}
     virtual ~RefCountObject() {}
 
-    void AddRef() { __sync_add_and_fetch(&m_refCount, 1); }
-    void Release() { if (__sync_sub_and_fetch(&m_refCount, 1) == 0) delete (DerivedClass*)this; }
+    void AddRef() {
+        __sync_add_and_fetch(&m_refCount, 1);
+    }
 
-    int GetRefCount() { return m_refCount; }
+    void Release() {
+        if (__sync_sub_and_fetch(&m_refCount, 1) == 0)
+            delete (DerivedClass*)this;
+    }
+
+    int GetRefCount() {
+        return m_refCount;
+    }
 
 private:
     int m_refCount;
+};
+
+template<class Object>
+class RefCountObjectPtr {
+public:
+    RefCountObjectPtr() : m_obj(NULL) {
+        pthread_mutex_init(&m_lock, NULL);
+    }
+
+    virtual ~RefCountObjectPtr() {
+        if (m_obj)
+            m_obj->Release();
+
+        pthread_mutex_destroy(&m_lock);
+    }
+
+    void Set(Object* obj) {
+        if (obj)
+            obj->AddRef();
+
+        pthread_mutex_lock(&m_lock);
+        Object* oldObj = m_obj;
+        m_obj = obj;
+        pthread_mutex_unlock(&m_lock);
+
+        if (oldObj)
+            oldObj->Release();
+    }
+
+    Object* Get() {
+        pthread_mutex_lock(&m_lock);
+        Object* obj = m_obj;
+        if (obj)
+            obj->AddRef();
+        pthread_mutex_unlock(&m_lock);
+
+        return obj;
+    }
+
+private:
+    Object* m_obj;
+    pthread_mutex_t m_lock;
 };
 
 #endif
--- cpp/ref_count/test.cpp
+++ cpp/ref_count/test.cpp
@@ -1,22 +1,104 @@
 #include "ref_count.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string>
 
-class MyObject : public RefCountObject<MyObject> {
+using namespace std;
+
+#define ARRAY_LEN           10000
+
+#define SET_THREADS         10
+#define SET_NULL_THREADS    3
+#define SET_TIMES           100000
+
+#define GET_THREADS         20
+#define GET_TIMES           100000
+
+class Data : public RefCountObject<Data> {
 public:
-    MyObject() { printf("Ctor\n"); }
-    ~MyObject() { printf("Dtor\n"); }
+    int i;
+    string s;
 };
 
+typedef RefCountObjectPtr<Data> DataPtr;
+
+DataPtr g_dataPtrs[ARRAY_LEN];
+
+int Rand(int bound) {
+    return rand() % bound;
+}
+
+Data* CreateData() {
+    Data* data = new Data();
+    data->i = rand();
+
+    char buf[6];
+    for(int i = 0; i < 5; ++i)
+        buf[i] = 'a' + Rand(26);
+    buf[5] = '\0';
+    data->s = buf;
+
+    return data;
+}
+
+void* SetThread(void* ptr) {
+    int tid = (long)ptr;
+    printf("Set thread %d start\n", tid);
+
+    for(int i = 0; i < SET_TIMES; ++i) {
+        Data* data = NULL;
+        if (tid >= SET_NULL_THREADS)
+            data = CreateData();
+
+        int index = Rand(ARRAY_LEN);
+        g_dataPtrs[index].Set(data);
+
+        if (data)
+            data->Release();
+    }
+
+    printf("Set thread %d end\n", tid);
+    return NULL;
+}
+
+void* GetThread(void* ptr) {
+    int tid = (long)ptr;
+    printf("Get thread %d start\n", tid);
+
+    char filename[256];
+    snprintf(filename, 256, "t%d.txt", tid);
+    FILE* f = fopen(filename, "w");
+    for(int i = 0; i < GET_TIMES; ++i) {
+        int index = Rand(ARRAY_LEN);
+        Data* data = g_dataPtrs[index].Get();
+        if (data) {
+            fprintf(f, "%d %s\n", data->i, data->s.c_str());
+            data->Release();
+        } else {
+            fprintf(f, "NULL\n");
+        }
+    }
+    fclose(f);
+
+    printf("Get thread %d end\n", tid);
+    return NULL;
+}
+
 int main() {
-    MyObject* myObj = new MyObject();
+    pthread_t setThreads[SET_THREADS];
+    pthread_t getThreads[GET_THREADS];
 
-    myObj->AddRef();
-    printf("%d\n", myObj->GetRefCount());
+    for(int i = 0; i < SET_THREADS; ++i)
+        pthread_create(&setThreads[i], NULL, SetThread, (void*)i);
+    for(int i = 0; i < GET_THREADS; ++i)
+        pthread_create(&getThreads[i], NULL, GetThread, (void*)i);
 
-    myObj->Release();
-    printf("%d\n", myObj->GetRefCount());
+    for(int i = 0; i < SET_THREADS; ++i)
+        pthread_join(setThreads[i], NULL);
+    for(int i = 0; i < GET_THREADS; ++i)
+        pthread_join(getThreads[i], NULL);
 
-    myObj->Release();
     return 0;
 }
 
