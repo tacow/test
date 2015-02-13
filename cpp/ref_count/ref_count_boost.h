5b67fb583c0a0a0075e3997a7e89622c89513008
yintao <yintao@localhost.localdomain>
2015-02-13 15:50:13 +0800

    ref_count_boost.h

--- cpp/ref_count/ref_count_boost.h
+++ cpp/ref_count/ref_count_boost.h
@@ -1,19 +1,23 @@
 #ifndef REF_COUNT_H
 #define REF_COUNT_H
 
+#include <boost/atomic.hpp>
+
+using namespace boost;
+
 template<class DerivedClass>
 class RefCountObject {
 public:
     RefCountObject(int refCount = 1) : m_refCount(refCount) {}
     virtual ~RefCountObject() {}
 
-    void AddRef() { __sync_add_and_fetch(&m_refCount, 1); }
-    void Release() { if (__sync_sub_and_fetch(&m_refCount, 1) == 0) delete (DerivedClass*)this; }
+    void AddRef() { ++m_refCount; }
+    void Release() { if (--m_refCount == 0) delete (DerivedClass*)this; }
 
     int GetRefCount() { return m_refCount; }
 
 private:
-    int m_refCount;
+    atomic_int m_refCount;
 };
 
 #endif
