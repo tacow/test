ff20c3302bb78729747f539ef2216fabdf2a55cf
yintao <taco@163.com>
2015-05-12 15:22:49 +0800

    httppost.py

--- python/httppost.py
+++ python/httppost.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
 import sys
-import urllib
-import httplib
+import urllib2
 
 if len(sys.argv) < 3:
-    print "Usage: %s [host] [message]" % sys.argv[0]
+    print "Usage: %s [url] [message]" % sys.argv[0]
     sys.exit()
-host = sys.argv[1]
+url = sys.argv[1]
 message = sys.argv[2]
 
 try:
-    params = urllib.urlencode({"message": message})
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    conn = httplib.HTTPConnection(host, timeout = 3)
-    conn.request(method = "POST", url = "/", body = params, headers = headers)
-    response = conn.getresponse()
-    print response.status, response.reason
+    request = urllib2.Request(url)
+    request.add_header("Content-Type", "text/plain")
+    response = urllib2.urlopen(url = request, data = message, timeout = 10)
+    print response.code, response.msg
     print response.read()
-    conn.close()
+    response.close()
 except Exception as e:
     print "Exception:", e
 
