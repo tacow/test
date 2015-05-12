30721e366aa34a39e546e32dacb2d9696b4739e7
yintao <taco@163.com>
2015-05-12 15:37:24 +0800

    httppost.py

--- python/httppost.py
+++ python/httppost.py
@@ -2,16 +2,21 @@
 import sys
 import urllib2
 
-if len(sys.argv) < 3:
-    print "Usage: %s [url] [message]" % sys.argv[0]
+if len(sys.argv) < 2:
+    print "Usage: %s [url] (data)" % sys.argv[0]
     sys.exit()
 url = sys.argv[1]
-message = sys.argv[2]
+data = None
+if len(sys.argv) >= 3:
+    data = sys.argv[2]
 
 try:
-    request = urllib2.Request(url)
-    request.add_header("Content-Type", "text/plain")
-    response = urllib2.urlopen(url = request, data = message, timeout = 10)
+    if data:
+        request = urllib2.Request(url)
+        request.add_header("Content-Type", "text/plain")
+        response = urllib2.urlopen(url = request, data = data, timeout = 10)
+    else:
+        response = urllib2.urlopen(url = url, timeout = 10)
     print response.code, response.msg
     print response.read()
     response.close()
