a7516908290f2cebc191340b47e80158c749dbb1
yintao <taco@163.com>
2015-05-12 10:52:27 +0800

    protobuf python

--- python/protobuf/test.py
+++ python/protobuf/test.py
@@ -16,19 +16,35 @@ def Write():
     f.write(person.SerializeToString())
     f.close()
 
+def Write2():
+    person = Person_pb2.Person()
+    person.age = 20
+    person.gender = Person_pb2.Female
+    person.married = False
+
+    f = open("Person.dat", "wb")
+    f.write(person.SerializeToString())
+    f.close()
+
 def Read():
     f = open("Person.dat", "rb")
     person = Person_pb2.Person()
     person.ParseFromString(f.read())
     f.close()
 
-    print person.name
-    print person.age
-    print person.gender
-    print person.married
+    if person.HasField("name"):
+        print "Name:", person.name
+    print "Age:", person.age
+    print "Gender:", person.gender
+    print "Married:", person.married
     for friend in person.friends:
-        print friend
+        print "Friend:", friend
 
 Write()
+Read()
+
+print "=============================="
+
+Write2()
 Read()
 
