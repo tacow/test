c8962df5c93872bc5acc3bd0de1f9df5333f2298
yintao <taco@163.com>
2015-06-15 16:37:03 +0800

    zlib.c

--- c/zlib.c
+++ c/zlib.c
@@ -57,17 +57,70 @@ void UncompressFile(const char* filename) {
     WriteFile(filename2);
 }
 
+void GzCompressFile(const char* filename) {
+    ReadFile(filename);
+
+    char filename2[256];
+    snprintf(filename2, 256, "%s.gz", filename);
+    gzFile gzf = gzopen(filename2, "wb");
+    if (!gzf) {
+        printf("Can't open file: %s\n", filename2);
+        return;
+    }
+    gzwrite(gzf, g_inData, g_inDataLen);
+    gzclose(gzf);
+}
+
+void GzUncompressFile(const char* filename) {
+    gzFile gzf = gzopen(filename, "rb");
+    if (!gzf) {
+        printf("Can't open file: %s\n", filename);
+        return;
+    }
+    g_outDataLen = gzread(gzf, g_outData, BUF_SIZE);
+    gzclose(gzf);
+
+    char filename2[256];
+    snprintf(filename2, 256, "%s.ungz", filename);
+    WriteFile(filename2);
+}
+
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Usage:\n");
-        printf("    %s <-d> [filename]\n", argv[0]);
+    const char* filename = NULL;
+    int uncompress = 0;
+    int gzipFormat = 0;
+
+    int opt = getopt(argc, argv, "ug");
+    while(-1 != opt) {
+        switch(opt) {
+        case 'u':
+            uncompress = 1;
+            break;
+        case 'g':
+            gzipFormat = 1;
+            break;
+        }
+        opt = getopt(argc, argv, "ug");
+    }
+    if (optind >= argc) {
+        printf("Usage: %s [-u] [-g] [filename]\n", argv[0]);
+        printf("  -u: uncompress\n");
+        printf("  -g: gzip format (default: zlib format)\n");
         return 0;
     }
+    filename = argv[optind];
 
-    if (argc > 2 && strcmp(argv[1], "-d") == 0)
-        UncompressFile(argv[2]);
-    else
-        CompressFile(argv[1]);
+    if (gzipFormat) {
+        if (uncompress)
+            GzUncompressFile(filename);
+        else
+            GzCompressFile(filename);
+    } else {
+        if (uncompress)
+            UncompressFile(filename);
+        else
+            CompressFile(filename);
+    }
 
     return 0;
 }
