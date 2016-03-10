08741411061a40474f9534fb40d79e68715678c7
yintao <taco@163.com>
2016-03-10 15:55:54 +0800

    parseurl

--- python/parseurl.py
+++ python/parseurl.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 import urlparse
 
-url = "http://www.example.com/path/query?a=1&b=2&b=3"
+url = "http://www.example.com/path/query?a=1&b=2&b=3&c="
 res = urlparse.urlparse(url)
 print res.scheme
 print res.netloc
 print res.path
-params = urlparse.parse_qs(res.query)
+params = urlparse.parse_qs(res.query, True)
 print params
